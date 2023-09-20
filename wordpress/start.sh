881247184e471953b2d60b9d22bd6e20f7bda53c
Yann de Thé <ydethe@gmail.com>
2023-09-20 09:31:25 +0000

    WIP wordpress swarm

--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
-sudo docker compose down
-sudo docker compose up -d --remove-orphans
+sudo docker stack rm wordpress
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack config --compose-file - 
+env $(cat .env | xargs) envsubst < ./docker-compose.yml | sudo docker stack deploy --compose-file - wordpress
+sudo docker service logs wordpress_wordpress -f
