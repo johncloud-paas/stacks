be7ce7c197cb64988a9b4b8e169af8730c3b451f
Yann de The <ydethe@gmail.com>
2023-12-01 10:24:27 +0100

    mdn2023 OK

--- wordpress/setup_after_up.sh
+++ wordpress/setup_after_up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-( source .env && chown -R 1001:1001 ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME} )
+# ( source .env && chown -R 1001:1001 ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME} )
--- wordpress/setup_before_up.sh
+++ wordpress/setup_before_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-( source .env &&
-  rm -rf ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME}/* &&
-  chown -R 1001:1001 ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME}
-)
+  # rm -rf ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME}/* &&
+# ( source .env &&
+#   chown -R 1001:1001 ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME}
+# )
