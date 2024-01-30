2118f12532819b943d299bebcde5d0d0a4a5c303
Yann de The <ydethe@gmail.com>
2024-01-30 20:59:57 +0100

    Portainer and backup only accessible via vpn

--- restart.sh
+++ restart.sh
@@ -5,6 +5,7 @@ sudo docker compose down &&
 (if test -f setup_before_up.sh; then
   sudo ./setup_before_up.sh
 fi) &&
+sudo docker compose pull &&
 sudo docker compose up -d --remove-orphans &&
 (if test -f setup_after_up.sh; then
   sudo ./setup_after_up.sh
