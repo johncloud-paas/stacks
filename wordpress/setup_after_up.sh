4e3251e36bb4eaeee9ec49089d1fd5214e312022
Yann de The <ydethe@gmail.com>
2024-02-08 12:42:35 +0100

    Added videos

--- wordpress/setup_after_up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# ( source .env && chown -R 1001:1001 ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME} )
--- wordpress/setup_before_up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-  # rm -rf ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME}/* &&
-# ( source .env &&
-#   chown -R 1001:1001 ${JOHNCLOUD_ROOT}/${COMPOSE_PROJECT_NAME}
-# )
