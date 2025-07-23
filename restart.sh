243765744e310561db5acdf8029e653822755385
Yann de The <ydethe@gmail.com>
2025-07-23 18:39:47 +0200

    Fixed string interpolation in restart script

--- restart.sh
+++ restart.sh
@@ -3,4 +3,5 @@
 cd $1 && \
 ( set -a; source .env; set +a; envsubst < docker-compose.yml ) > /tmp/$1-docker-compose.yml && \
 # sudo docker compose pull && \
-sudo docker stack deploy --compose-file /tmp/$1-docker-compose.yml $1
+sudo docker stack rm $1 --detach=false && \
+sudo docker stack deploy --compose-file /tmp/$1-docker-compose.yml --detach=false $1
